8901342beb947ffdb813d46101a2cd0522ed5783
ammarshaikh123 <ammarshaikh50099@gmail.com>
2024-02-02 10:53:14 -0700

    t

--- js/script.js
+++ js/script.js
@@ -5,12 +5,10 @@
 const menu = document.querySelector('.menu');
 const icon = document.querySelector('.menu-icon');
 const cross_bar = document.querySelector('.cross-bar');
-
 icon.addEventListener('click', () => {
     menu.classList.add('d-menu')
     cross_bar.classList.add('d-cross')
 });
-
 cross_bar.addEventListener('click', () => {
     menu.classList.remove('d-menu')
     cross_bar.classList.remove('d-cross')
@@ -22,9 +20,7 @@ cross_bar.addEventListener('click', () => {
 
 const counter = document.querySelectorAll('.counter');
 let speed = 200;
-
 counter.forEach((currElem) => {
-
     const updateCounter = () => {
         const dataTarget = parseInt(currElem.dataset.num)
         const initialValue = parseInt(currElem.innerText)
@@ -34,6 +30,5 @@ counter.forEach((currElem) => {
             setTimeout(updateCounter, 10)
         }
     }
-
     updateCounter();
 });
